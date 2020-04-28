a5f0a6cc7fd5d9884031225a95a8791dffa396be
alexhruzd <alex.gaby77@gmail.com>
2020-04-28 17:07:08 +0300

    create gallery

---
+++ js/main.js
@@ -0,0 +1,41 @@
+let gallery = document.querySelector('.gallery');
+gallery.addEventListener('click', showModal);
+
+let modalWrapper = document.querySelector('.modal_wrapper');
+let modal = document.querySelector('.modal_block');
+
+let modalSlider = document.querySelector('.modal_slider');
+let modalClose = document.querySelector('.modal_close');
+
+
+modalClose.addEventListener('click', (e) => {
+
+     modalWrapper.classList.remove('active');
+});
+
+
+function showModal(event) {
+     let numImg = 0;
+     let curImg = event.target.closest('div');
+
+     for (let i = 0; i<gallery.children.length; i++) {
+          if (curImg === gallery.children[i]) {
+               numImg = i;
+               break;
+          }
+     }
+
+     modalWrapper.classList.add('active');
+
+     modalSlider.innerHTML = gallery.innerHTML;
+     modalSlider.slider(sliderSettings = {
+          slideWidth: 1000,
+          autoplay: false,
+          dots: false,
+          margin: 0
+     }, numImg);
+
+     modalSlider.querySelector('.navs').classList.add('modal_navs');
+}
+
+
